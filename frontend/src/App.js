f9e6db83f1b4085da6cfe17888cfe7c65b843024
bikramsubedi <bxs1561@g.rit.edu>
2021-01-20 21:25:04 -0500

    implement add to cart and store all the items that are added to cart in local storage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,18 +10,28 @@ import {
 import ProductScreen from "./screens/ProductScreen";
 import HomeScreen from "./screens/HomeScreen";
 import CartScreen from "./screens/CartScreen";
+import {useSelector} from "react-redux";
 
 function App() {
+    const cart = useSelector(state => state.cart);
+    const {cartItems} = cart;
   return (
       <Router>
           <div className="grid-container">
               <header className="row">
                   <div>
-                      <a className="brand" href="index.html">amazon</a>
+                      <Link className="brand" to="/">amazon</Link>
                   </div>
                   <div>
-                      <a href="/cart">Cart</a>
-                        <a href="/signin">Sign In</a>
+                      <Link to="/cart">Cart</Link>
+                      {
+                          cartItems.length>0&&(
+                              <span className="badge">
+                                  {cartItems.length}
+                              </span>
+                          )
+                      }
+                        <Link to="/signin">Sign In</Link>
                   </div>
               </header>
               <main>
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, {useEffect} from "react";
+import {addToCart} from "../actions/cartActions";
+import {useDispatch} from "react-redux";
 
 
 function CartScreen(props) {
+    const dispatch = useDispatch();
     const productId = props.match.params.id;
     const qty = props.location.search? Number(props.location.search.split('=')[1]):1;
+
+    useEffect(()=>{
+        if(productId){
+            dispatch(addToCart(productId, qty))
+        }
+    },[dispatch, productId, qty]);
     return(
         <div>
             <h1>Cart Screen</h1>
