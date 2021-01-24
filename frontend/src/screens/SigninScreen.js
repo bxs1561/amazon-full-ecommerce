fd38b843e31881751cf405a0abdc51cc7c190010
bikramsubedi <bxs1561@g.rit.edu>
2021-01-24 18:34:26 -0500

    fix localstoare errors, completed user register fron and back end

--- frontend/src/screens/SigninScreen.js
+++ frontend/src/screens/SigninScreen.js
@@ -1,19 +1,35 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {signin} from "../actions/userActions";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
 
-function SigninScreen() {
+function SigninScreen(props) {
     const[email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const userSignin = useSelector(state=>state.userSignin);
+    const {userInfo, loading, error} = userSignin;
+
+    const dispatch = useDispatch();
+    const redirect = props.location.search? props.location.search.split("=")[1] : "/";
     const submitHandler =(e)=>{
         e.preventDefault();
-
-    }
+        dispatch(signin(email, password))
+    };
+    useEffect(()=>{
+        if(userInfo){
+            props.history.push(redirect)
+        }
+    },[props.history,redirect, userInfo])
     return(
         <div>
             <form className="form" onSubmit={submitHandler}>
             <div>
                 <h1>Sign In</h1>
             </div>
+                {loading && <LoadingBox/>}
+                {error && <MessageBox variant="danger">{error}</MessageBox>}
             <div>
                 <label htmlFor="email">Email</label>
                 <input
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,8 +4,14 @@ import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import thunk from "redux-thunk";
 import {productDetailsReducer, productListReducer} from "./reducers/productReducers";
 import {cartReducer} from "./reducers/cartReducers";
+import {userSigninReducer} from "./reducers/userReducers";
 
 const initialState = {
+    userSignin:{
+        userInfo: localStorage.getItem("userInfo")?
+            JSON.parse(localStorage.getItem("userInfo")):
+            []
+    },
     cart: {
         cartItems: localStorage.getItem('cartItems')?
             JSON.parse(localStorage.getItem('cartItems')):
@@ -16,6 +22,7 @@ const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
+    userSignin: userSigninReducer
 })
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__|| compose
 
