88b5e6efa153b17b67e942b2b0751c66229e9702
bikramsubedi <bxs1561@g.rit.edu>
2021-02-05 19:59:51 -0500

    add order history get request and front screen and add paypal payment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import mongoose from "mongoose";
 import userRouter from "./routers/userRouter.js";
 import productRouter from "./routers/productRouter.js";
+import orderRouter from "./routers/OrderRouter.js";
 
 import dotenv from "dotenv";
 dotenv.config();
@@ -42,7 +43,8 @@ app.get("/",(req,res)=>{
     res.send("server is ready")
 });
 app.use("/api/users",userRouter);
-app.use("/api/products",productRouter)
+app.use("/api/products",productRouter);
+app.use("/api/orders",orderRouter);
 
 app.use((err, req, res, next)=>{
     res.status(500).send({message: err.message})
